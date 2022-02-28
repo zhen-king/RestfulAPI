97d4edca370d1597b1cd2599f2c183a4dfbd7aca
Zhen <77253245+zhen-king@users.noreply.github.com>
2022-02-28 13:53:05 -0500

    Delete Student directory

--- Student/src/main/java/com/example/student/Exception/StudentNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.student.Exception;
- public class StudentNotFoundException extends RuntimeException{
-     public StudentNotFoundException(Long id) {
-      super("Could not find employee " + id);
-     }
-}
